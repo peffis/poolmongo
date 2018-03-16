f387aefb82b53b2d6098777417b3b89594bf4822
Stefan Hellkvist <hellkvist@gmail.com>
2018-03-16 14:00:02 +0100

    adding support for generic commands

--- src/pmongo.erl
+++ src/pmongo.erl
@@ -1,5 +1,5 @@
 %% @doc The poolmongo db api
-%% This is a thin wrapper on top of the Erlang mongodb 
+%% This is a thin wrapper on top of the Erlang mongodb
 %% that shows how one can use it together with poolboy
 %% @end
 %% @author Stefan Hellkvist.
@@ -8,7 +8,7 @@
 
 -module(pmongo).
 -export([
-	 insert/2, 
+	 insert/2,
 	 insert/3,
 	 update/3,
 	 update/5,
@@ -20,8 +20,9 @@
 	 find_one/3,
 	 find/2,
 	 find/3,
-	 count/2, 
-	 count/3
+	 count/2,
+	 count/3,
+         command/1
 	]).
 
 -define(POOL_NAME, pmongo).
@@ -29,153 +30,157 @@
 
 %% @doc insert a Doc into a collection.
 insert(Collection, Doc) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:insert(Worker, Collection, Doc) 
+				mc_worker_api:insert(Worker, Collection, Doc)
 			end).
 
 %% @doc insert a Doc into a collection.
 insert(Collection, Doc, WC) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:insert(Worker, 
-						     Collection, 
-						     Doc, 
-						     WC) 
+				mc_worker_api:insert(Worker,
+						     Collection,
+						     Doc,
+						     WC)
 			end).
 
 
 
 %% @doc Replace the document matching criteria entirely with the new Document.
 update(Coll, Selector, Doc) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:update(Worker, 
-						     Coll, 
+				mc_worker_api:update(Worker,
+						     Coll,
 						     Selector,
-						     Doc) 
+						     Doc)
 			end).
 
 
 %% @doc Replace the document matching criteria entirely with the new Document.
 update(Coll, Selector, Doc, Upsert, MultiUpdate) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:update(Worker, 
-						     Coll, 
+				mc_worker_api:update(Worker,
+						     Coll,
 						     Selector,
-						     Doc, 
-						     Upsert, 
-						     MultiUpdate) 
+						     Doc,
+						     Upsert,
+						     MultiUpdate)
 			end).
 
 
 %% @doc Replace the document matching criteria entirely with the new Document.
 update(Coll, Selector, Doc, Upsert, MultiUpdate, WC) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:update(Worker, 
-						     Coll, 
+				mc_worker_api:update(Worker,
+						     Coll,
 						     Selector,
-						     Doc, 
-						     Upsert, 
-						     MultiUpdate, 
-						     WC) 
+						     Doc,
+						     Upsert,
+						     MultiUpdate,
+						     WC)
 			end).
-    
+
 
 % @doc deletes document from Collection matching Selector.
 delete(Coll, Selector) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:delete(Worker, 
-						     Coll, 
+				mc_worker_api:delete(Worker,
+						     Coll,
 						     Selector)
 			end).
-    
+
 % @doc deletes first selected document.
 delete_one(Coll, Selector) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:delete_one(Worker, 
-							 Coll, 
+				mc_worker_api:delete_one(Worker,
+							 Coll,
 							 Selector)
 			end).
 
 
 % @doc deletes first N selected document.
 delete_limit(Coll, Selector, N) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:delete_limit(Worker, 
-							   Coll, 
-							   Selector, 
+				mc_worker_api:delete_limit(Worker,
+							   Coll,
+							   Selector,
 							   N)
 			end).
 
 
 %% @doc returns first selected document, if any
 find_one(Coll, Selector) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:find_one(Worker, 
-						       Coll, 
+				mc_worker_api:find_one(Worker,
+						       Coll,
 						       Selector)
 			end).
 
 
 %% @doc returns first selected document, if any
 find_one(Coll, Selector, Args) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:find_one(Worker, 
-						       Coll, 
-						       Selector, 
+				mc_worker_api:find_one(Worker,
+						       Coll,
+						       Selector,
 						       Args)
 			end).
-    
-    
-%% @doc Return selected documents. 
+
+
+%% @doc Return selected documents.
 find(Coll, Selector) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:find(Worker, 
-						   Coll, 
+				mc_worker_api:find(Worker,
+						   Coll,
 						   Selector)
 			end).
 
 
-%% @doc Return selected documents. 
+%% @doc Return selected documents.
 find(Coll, Selector, Args) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:find(Worker, 
-						   Coll, 
-						   Selector, 
+				mc_worker_api:find(Worker,
+						   Coll,
+						   Selector,
 						   Args)
 			end).
-    
 
-%% @doc Count selected documents. 
+
+%% @doc Count selected documents.
 count(Coll, Selector) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:count(Worker, 
-						    Coll, 
+				mc_worker_api:count(Worker,
+						    Coll,
 						    Selector)
 			end).
 
 
 %% @doc Count selected documents up to given max number, 0 means no max
 count(Coll, Selector, Args) ->
-    poolboy:transaction(?POOL_NAME, 
+    poolboy:transaction(?POOL_NAME,
 			fun(Worker) ->
-				mc_worker_api:count(Worker, 
-						    Coll, 
-						    Selector, 
+				mc_worker_api:count(Worker,
+						    Coll,
+						    Selector,
 						    Args)
 			end).
 
 
-    
-    
+%% @doc Execute a generic command on the mongodb server
+command(Command) ->
+    poolboy:transaction(?POOL_NAME,
+                        fun(Worker) ->
+                                mc_worker_api:command(Worker, Command)
+                        end).
